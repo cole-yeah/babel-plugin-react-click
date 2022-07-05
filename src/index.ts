4b1f8cf1b2af38e404fd8b562446241e7aa03dcc
chenlecheng <243461254@qq.com>
2022-07-05 20:58:22 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ import { Visitor, NodePath } from "@babel/traverse";
 
 const BASE_ATTR_NAME = "onClick";
 
+let timer: NodeJS.Timeout = null;
+const debounce = (fn, delay = 200) => {
+  return (...args) => {
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
+
 // 根据onClick属性获取函数值
 const getTargetValue = (jsxElement: NodePath) => {
   const { attributes } = jsxElement.openingElement;
@@ -18,13 +28,19 @@ const getTargetValue = (jsxElement: NodePath) => {
   return value;
 };
 
-const babelPluginReactClick = () => {
+const checkType = (value: t.JSXExpressionContainer) => {
+  if (t.isArrowFunctionExpression(value)) {
+  }
+};
+
+const babelPluginReactClick = ({ types: t }) => {
   const visitor: Visitor = {
     JSXElement(path: NodePath, state) {
       const { node } = path;
       const value = getTargetValue(node);
       if (!value) return;
       // TODO: 怎么对value里面的函数进行增加防抖替换呢？
+      checkType(value);
     },
   };
   return {
